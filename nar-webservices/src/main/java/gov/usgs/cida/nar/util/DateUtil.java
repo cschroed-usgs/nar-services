cd5d98ed4c9efa79111c0510a1ef178bdc9c8945
Jordan Walker <jiwalker@usgs.gov>
2016-01-27 12:24:55 -0600

    Adding support for date format UI is passing in

--- nar-webservices/src/main/java/gov/usgs/cida/nar/util/DateUtil.java
+++ nar-webservices/src/main/java/gov/usgs/cida/nar/util/DateUtil.java
@@ -1,6 +1,8 @@
 package gov.usgs.cida.nar.util;
 
 import java.sql.Date;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
 
@@ -11,7 +13,8 @@ import org.joda.time.format.ISODateTimeFormat;
  */
 public class DateUtil {
 	
-	private static final DateTimeFormatter dateTimeParser = ISODateTimeFormat.dateTimeParser().withOffsetParsed();
+	private static final DateTimeFormatter isoDateTimeParser = ISODateTimeFormat.dateTimeParser().withOffsetParsed();
+	private static final DateTimeFormatter nonStandardDateTimePaser = DateTimeFormat.forPattern("MM/dd/yyyy");
 	
 	/**
 	 * Get water year from date string
@@ -21,7 +24,7 @@ public class DateUtil {
 	public static Integer getWaterYear(String dateStr) {
 		Integer wy = null;
 		if (null != dateStr) {
-			wy = dateTimeParser.parseDateTime(dateStr).plusMonths(3).getYear();
+			wy = multiParse(dateStr).plusMonths(3).getYear();
 		}
 		return wy;
 	}
@@ -35,8 +38,18 @@ public class DateUtil {
 	public static Date getSqlDate(String dateStr) {
 		Date date = null;
 		if (null != dateStr) {
-			date = new Date(dateTimeParser.parseDateTime(dateStr).getMillis());
+				date = new Date(multiParse(dateStr).getMillis());
 		}
 		return date;
+	}
+		
+	private static DateTime multiParse(String dateStr) {
+		DateTime dt = null;
+		try {
+			dt = isoDateTimeParser.parseDateTime(dateStr);
+		} catch (IllegalArgumentException ex) {
+			dt = nonStandardDateTimePaser.parseDateTime(dateStr);
+		}
+		return dt;
 	}
 }
