1614ab34c3ab184f01f776848596b76ab6d6af22
Carl Schroedl <cschroedl@usgs.gov>
2016-02-02 10:11:58 -0600

    test cleanup

--- nar-webservices/src/test/java/gov/usgs/cida/nar/domain/TimeSeriesAvailabilityTest.java
+++ nar-webservices/src/test/java/gov/usgs/cida/nar/domain/TimeSeriesAvailabilityTest.java
@@ -1,23 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gov.usgs.cida.nar.domain;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.force66.beantester.BeanTester;
 import org.joda.time.DateTime;
 
-/**
- *
- * @author cschroed
- */
 public class TimeSeriesAvailabilityTest {
 	
 	@Test
